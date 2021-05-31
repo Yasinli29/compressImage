5b77c3f314ba7a42a31d4f8d0c0c71cdf2c22331
Yasinli29 <44838767+Yasinli29@users.noreply.github.com>
2021-05-31 11:29:44 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,81 @@
+export default class {
+  constructor(file, options = {}) {
+    this.file = file
+    this.options = options
+  }
+
+  async getDrewCanvas() {
+    const imageTag = await this.getImageTag()
+    const { width, height } = imageTag
+    const {
+      scale = 1,
+      width: targetWidth = width * scale,
+      height: targetHeight = height * scale
+    } = this.options
+
+    const oCanvas = document.createElement('canvas')
+    const ctx = oCanvas.getContext('2d')
+
+    oCanvas.width = targetWidth
+    oCanvas.height = targetHeight
+
+    ctx.clearRect(0, 0, targetWidth, targetHeight)
+    ctx.drawImage(imageTag, 0, 0, targetWidth, targetHeight)
+    return oCanvas
+  }
+
+  getChangedImageTag() {
+    return new Promise(async (resolve, reject) => {
+      const newImage = new Image()
+      newImage.onload = () => resolve(newImage)
+      newImage.onerror = err => reject(err)
+      newImage.src = await this.getChangedBase64()
+    })
+  }
+
+  async getChangedBase64() {
+    const {
+      fileType = this.file.type,
+      quality
+    } = this.options
+    const oCanvas = await this.getDrewCanvas()
+    return oCanvas.toDataURL(fileType, quality)
+  }
+
+  getChangedFile() {
+    return new Promise(async (resolve) => {
+      const {
+        fileType = this.file.type,
+        quality
+      } = this.options
+
+      const oCanvas = await this.getDrewCanvas()
+
+      oCanvas.toBlob((blob) => {
+        resolve(new File([blob], this.file.name, {
+          type: blob.type
+        }))
+      }, fileType, quality)
+    })
+  }
+
+  getImageTag() {
+    return new Promise(async (resolve, reject) => {
+      const newImage = new Image()
+      newImage.onload = () => resolve(newImage)
+      newImage.onerror = err => reject(err)
+      newImage.src = await this.getBase64()
+    })
+  }
+
+  getBase64() {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        resolve(e.target.result)
+      }
+      reader.onerror = err => reject(err)
+      reader.readAsDataURL(this.file)
+    })
+  }
+}
